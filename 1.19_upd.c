90ca7da624aa19007cc2d352952730cdb8eed9cf
Alexdark19974 <90026506+Alexdark19974@users.noreply.github.com>
2021-11-28 12:41:52 +0300

    Add files via upload
    List of changes:
    1) reversal of line after pushing ENTER 
    2) after ENTER, program awaits antother line to reverse
    3) if line consists of 1 \n, it becomes \0, and the output is : "Nothing to reverse. Terminated."

---
+++ 1.19_upd.c
@@ -0,0 +1,57 @@
+
+	
+//Write a function reverse(s) that reverses the character string s . Use it to write a program that reverses its input a line at a time.
+
+#include <stdio.h>
+#define MAXL 1000
+void reverse(char[], int);
+int get_line(char[], int);
+int main (void)
+{
+    char s[MAXL];
+    int length = 0;
+    while ((length = get_line(s, MAXL)) > 0)
+    {
+        if (length > 0)
+        {
+            reverse(s, length);
+            printf("%s", s);
+        }
+    }
+    return 0;
+}
+void reverse (char s[], int length)
+{
+    char temp;
+    int i = 0;
+    for (i = 0; i < length/2; i++)
+    {
+        temp = s[i];
+        s[i] = s[length - 1 - i];
+        s[length - 1 - i] = temp;
+    }
+    s[length] = '\n';
+    s[length + 1] = '\0';
+}
+int get_line(char s[], int MAXLINE)
+{
+    int c;
+    int i = 0;
+    for (i = 0; i < MAXLINE - 2 && (c = getchar()) != EOF && c != '\n';)
+    {
+        s[i] = c;
+        i++;
+    }
+    if (c == '\n' && i == 0)
+    {
+        s[i] = '\0';   
+        printf("Nothing to reverse. Terminated.\n");
+        return 0;
+    }
+    else if (c == EOF)
+    {
+        s[i] = '\0';
+        puts("");
+    }
+    return i;
+}
