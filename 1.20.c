03f6431d77bb4a6c71afa030fa8b24f0cb8d8833
Alexdark19974 <90026506+Alexdark19974@users.noreply.github.com>
2021-11-29 09:48:14 +0300

    Add files via upload
    Write a program detab that replaces tabs in the input with the proper number of blanks to space to the next tab stop. Assume a fixed set of tab stops, say every n columns. Should n be a variable or a symbolic parameter?

---
+++ 1.20.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#define MAXL 1000
+#define TAB 8
+int get_line(char[], int);
+void copy(char[], char[]);
+int main(void)
+{
+    char s[MAXL];
+    char s_edited[MAXL];
+    int len;
+    while ((len = get_line(s, MAXL)) > 0)
+    {
+        if (len > 0)
+        {
+            copy(s_edited, s);
+            printf("%s", s_edited);   
+        }
+    }
+    return 0;
+}
+int get_line(char s[], int max)
+{
+    int c, i = 0;
+    for (i = 0; i < MAXL - 2 && (c = getchar()) != EOF && c != '\n'; i++)
+    {
+        s[i] = c;
+    }
+    if (c == '\n')
+    {
+        s[i] = c;
+    }
+    else if (c == EOF)
+    {
+        s[i] = '\0';
+        puts("");
+        return i;
+    }
+    s[i++] = '\0';
+    return i;
+}
+void copy(char s_edited[], char s[])
+{
+    int i = 0;
+    int count = 0;
+    int j = 0;
+    while (s[j] != '\0')
+    {
+        if (s[j] == '\t')
+        {
+            int res = TAB - (count % TAB);
+            while (res > 0)
+            {
+                s_edited[i] = ' ';
+                i++;
+                res--;
+            }
+            j++;
+            count = 0;
+        }
+        else
+        {
+            s_edited[i] = s[j];
+            i++;
+            j++;
+            count++;
+        }
+    }
+    s_edited[i++] = '\n';
+    s_edited[i++] = '\0';
+}
