8253f933b663e071b06059225795959e3bd10077
Alexdark19974 <90026506+Alexdark19974@users.noreply.github.com>
2021-12-01 07:12:12 +0300

    Update 1.21.c
    cosmetic changes.

--- 1.21.c
+++ 1.21.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 #define MAXL 1000
 #define TAB 8
-/* Write a program entab that replaces strings of blanks with the minimum number of tabs and blanks to achieve the same spacing. 
-  Use the same stops as for detab.
- When either a tab or a single blank would suffice to reach a tab stop, which should be given preference? */
+
 int get_line (char[], int);
 void entab (char[], char[]);
+
 int main (void)
 {
-    char s[MAXL];
-    char s_edited[MAXL];
+    char s[MAXL], s_edited[MAXL];
     int len;
     while ((len = get_line(s, MAXL)) > 0)
     {
-        if (len > 0)
-        {
-            entab(s_edited, s);
-            printf("%s", s_edited);   
-        }
+        entab(s_edited, s);
+        printf("%s", s_edited);   
     }
     return 0;
 }
 int get_line (char s[], int max)
 {
     int c, i = 0;
+
     for (i = 0; i < MAXL - 2 && (c = getchar()) != EOF && c != '\n'; i++)
     {
         s[i] = c;
@@ -44,11 +40,9 @@ int get_line (char s[], int max)
 }
 void entab (char s_edited[], char s[])
 {
-    int i = 0;
+    int i = 0, j = 0, b_count = 0, t_count = 0;
     int s_count = 1;
-    int b_count = 0;
-    int t_count = 0;
-    int j = 0;
+
     while (s[j] != '\0')
     {
         if (s[j] == ' ')
@@ -88,9 +82,16 @@ void entab (char s_edited[], char s[])
                     b_count--;
                 }
             }
-                s_edited[i] = s[j];
-                i++;
-                j++;
+
+            s_edited[i] = s[j];
+
+            if (s[j] == '\t')
+            {
+                s_count = TAB - (s_count % TAB); 
+            }
+            i++;
+            j++;
+            
         }
         s_count++;
     }
