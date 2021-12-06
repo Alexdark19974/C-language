3d3d2fcc02ff72507c7f47cd253c4442bf9f3d6c
Alexdark19974 <90026506+Alexdark19974@users.noreply.github.com>
2021-12-06 06:32:08 +0300

    Update 1.22.c
    The following program performs word wrap function, similar to VSC word wrap in settings.
    1. get_line is basically fgets, which also checks if there are any \t, in case of which replaces it with the number of blank spaces necessary to emulate \t.
    2. fold is where word wrap happens. Upon hiting the 6 F_TRIGGER mark, we split the line.
    3.  If the word is very long, then we split the word itself, put \n in between the seperated line, and move on.

--- 1.22.c
+++ 1.22.c
@@ -1,9 +1,11 @@
+ 
 #include <stdio.h>
 #define MAXS 1000
 #define F_TRIGGER 6
 #define TAB 8
 
-int get_line(char[], int);
+int
+get_line(char[], int);
 void fold (char[], char[]);
 
 int main (void)
@@ -14,7 +16,7 @@ int main (void)
 
     while ((length = get_line(string, MAXS)) > 0)
     {
-        fold (f_string, string);
+        fold(f_string, string);
         printf("%s", f_string);
     }
     return 0;
@@ -24,11 +26,28 @@ int get_line(char string[], int maxs)
 {
     int i = 0;
     int c;
+    int tab_count = 0;
 
     while (i < maxs - 2 && (c = getchar()) != EOF && c != '\n')
     {
-        string[i] = c;
-        i++;
+        if (c == '\t')
+        {
+            int res = TAB - (tab_count % TAB);
+
+            while (res > 0)
+            {
+                string[i] = ' ';
+                i++;
+                res--;
+            }
+            tab_count = 0;
+        }
+        else
+        {
+            string[i] = c;
+            i++;
+            tab_count++;
+        }
     }
     if (c == '\n')
     {
@@ -46,34 +65,74 @@ void fold(char to[], char from[])
     int j = 0;
     int pos = 0;
     int b_pos = 0;
-    int b_previous = 0;
-    int first_blank = 0;
 
-    while (from[j] != '\0')
+    while (from[i] != '\0')
     {
-         if (pos == F_TRIGGER)
-         {
-             if (from[j] != ' ')
-             {
-                while (from[j] != ' ')
+        if (pos >= F_TRIGGER)
+        {
+            if (from[i] != ' ')
+            {
+                if (b_pos == 0)
                 {
-                     to[i] = from[j];
-                     i++;
-                     j++;
+                    b_pos = F_TRIGGER;
+
+                    while (j < b_pos)
+                    {
+                        to[j] = from[j];
+                        j++;
+                    }
+                    to[j] = '\n';
+                    j++;
+                    to[j] = from[b_pos];
+                    j = b_pos + 1;
+                    i = b_pos;
+                    pos = 0;
+                    b_pos = 0;
+                }
+                else
+                {
+                    while (j < b_pos)
+                    {
+                        to[j] = from[j];
+                        j++;
+                    }
+                    to[b_pos] = '\n';
+                    pos = 0;
+                    i = b_pos + 1;
+                    j = b_pos + 1;
                 }
             }
-            to[b_pos] = '\n';
-            pos = 0;
-        }
-            to[i] = from[j];
-            
-            if (from[j] == ' ' && from[j] != '\n')
+            else
             {
-                b_pos = j;   
-                // 3 7 13 16 20   16 - 3 = 13 
-               // printf("%d ", b_pos);
+                b_pos = i;
+                
+                while (j < b_pos)
+                {
+                    to[j] = from[j];
+                    j++;
+                }
+                to[b_pos] = '\n';
+                pos = 0;
+                i = b_pos + 1;
+                j = b_pos + 1;
             }
-             i++, j++, pos++;
+        }
+        if (from[i] == ' ')
+        {
+            b_pos = i;
+        }
+        else if (from[i] == '\n')
+        {
+          while (j <= i)
+                {
+                    to[j] = from[j - 1];
+                    j++;
+                }    
+        }
+        i++;
+        pos++;
     }
-    to[i] = '\0';
+    to[j] = '\n';
+    j++;
+    to[j] = '\0';
 }
