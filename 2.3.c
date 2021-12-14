b32b1736bc3e3c5d31140fbb1f8f629e992724fa
Alexdark19974 <90026506+Alexdark19974@users.noreply.github.com>
2021-12-14 09:30:57 +0300

    Add files via upload
    The following program tackles the issue of converting hexidecimals into decimals.
    V1 allows to input numbers from 0 to 9 without getting an error.
    V2 allows to input only hexidecimals starting with 0x/0X.
    The latter is shorter, the former is longer but a bit more lenient.

---
+++ 2.3.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <ctype.h>
+#define LIMIT 102
+/*Write the function htoi(s) which converts a string of hexadecimal digits (including an optional 0x, 0X) into its equivalent integer value. Allowable digits are 0..9, a through f, A through F.*/
+void get_line (char[], int);
+void atoi(char[]);
+void htoi(char[]);
+int main(void)
+{
+    char s[LIMIT];
+    get_line(s, LIMIT);
+    htoi(s);
+
+    return 0;
+}
+void htoi(char s[])
+{
+    int i = 0;
+    char map_of_hex[LIMIT] = "0123456789abcdef\0";
+    int formula = 0;
+    
+    if (s[i] >= '0' && s[i] <= '9' && s[i + 1] != 'x' && s[i + 1] == '\0')
+        {
+            atoi(s);
+            return;
+        }
+    else if (s[i] == '0' && s[i + 1] == 'x')
+        {
+            i++;
+            i++;
+            
+            while (s[i] != '\0')
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    if (s[i] == map_of_hex[j])
+                    {
+                        formula = formula * 16 + j;
+                        if (s[i + 1] == '\0')
+                        {
+                            printf("%d", formula);
+                        }
+                        break;
+                    }
+                }
+                i++;
+            }
+        }
+        else
+        {
+            printf("Invalid hexidecimal number\n");
+            return;
+        }
+}
+
+void get_line (char s[], int max)
+{
+    int c, i = 0;
+    for (i = 0; i < LIMIT - 2 && (c = getchar()) != EOF && c != '\n'; i++)
+    {
+        if (c >= 'A' && c <= 'Z')
+        {
+            c = tolower(c);
+        }
+        s[i] = c;
+    }
+    s[i] = '\0';
+    return;
+}
+void atoi (char s[])
+{
+    int i;
+    int n = 0;
+    for (i = 0; s[i] >= '0' && s[i] <= '9'; i++)
+    {
+        n = 10 * n + (s[i] - '0');
+    }
+    printf("%d ", n);
+}
