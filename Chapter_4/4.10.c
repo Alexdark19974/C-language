b6f91b840ec7a003afb6539bacd4c860f376f492
Alexdark19974 <voitexman11@gmail.com>
2023-02-04 09:27:49 +0700

    Refactored code in compliance with K&R C-style

--- Chapter_4/4.10.c
+++ Chapter_4/4.10.c
@@ -1,308 +1,249 @@
-  
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include <stdlib.h>  /* for atof() */
 #include <math.h>
-#define MAXOP 100
-#define MAXVAL 100
-#define BUFSIZE 100
-#define NUMBER '0'
-#define MYEOF '1'
+#include <ctype.h>
 
-int sp = 0;
-double val[MAXVAL];
-char buf[BUFSIZE];
-char s[MAXVAL];
-int bufp = 0;
-int flag = 0;
-int close_get_line = 0;
-
-
-int getop(char[]);
+#define MAXLINE 1000
+#define MAXOP   100  /* max size of operand or operator */
+#define NUMBER  '0'  /* signal that a number was found */
+#define MODULUS '%'
+#define PRINT_TOP 't'
+#define CLEAR_STACK 'c'
+#define COSINUS 'C'
+#define SINUS 'S'
+#define EXPONENT 'e'
+#define DUPLICATE_STACK 'd'
+#define POW '^'
+int getop(char [], char[]);
+int get_line(char [], int lim);
 void push(double);
 double pop(void);
-void get_line(char *, int);
-double stack_top(void);
-void dublicate(void);
-void swap(void);
-void free_stack(void);
-
-int main(void)
+void print_top(void);
+void clear_stack(void);
+void duplicate_stack(void);
+int handle_variables(char []);
+int is_EOF = 0;
+int i = 0;
+/* reverse Polish calculator */
+main()
 {
-    int type = 0;
+    int type, len;
     double op2;
-    int op_2;
-    int op_1;
-    char s[MAXOP];
-    int lock = 0;
-    double variable;
-    char buff_line[MAXVAL];
+    char s[MAXOP] = {0};
+    char digit[MAXOP] = {0};
+    printf(": ");
 
-    while ((type = getop(buff_line)) != EOF)
-    {
-        switch(type)
-        {
-            case MYEOF:
-            {
-                printf ("\t%.8g\n", pop());
-                return 0;
-            }
-            case 'a': case 'b' : case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
-            {
-                variable = pop();
-                printf("the value of the latest printed variable \'%c\' is %.8g\n", (char) type, variable);
-                lock = 1;
-                break;
-            }
-            case NUMBER:
-            {
-                push(atof(buff_line));
-                printf("%s s is buff_line\n", buff_line);
-                break;
-            }
-            case '+':
-            {
-
-                push(pop() + pop());
-                break;
-            }
-            case '-':
-            {
-                op2 = pop();
-                push(pop() - op2);
-                break;
-            }
-            case '*':
-            {
-                push(pop() * pop());
-                break;
-            }
-            case '/':
-            {
-                op2 = pop();
-
-                if (op2 != 0.0)
-                {
-                    push(pop() / op2);
-                }
-                else
-                {
-                    printf("error: zero divisor\n");
-                }
-                break;
-            }
-            case '%':
-            {
-                op_2 = pop();
-                op_1 = pop();
-                push(op_1 % op_2);
-                break;
-            }
-            case '?':
-            {
-                printf("the top of the stack is %.8g\n", stack_top());
-                lock = 1;
-                break;
-            }
-            case '!':
-            {
-                dublicate();
-                printf("%.8g is the dublicate of the stack's top %.8g\n", pop(), pop());
-                lock = 1;
-                break;
-            }
-            case '>':
-            {
-                swap();
-                printf("the result of swap is %.8g and %.8g\n", pop(), pop());
-                lock = 1;
-                break;
-            }
-            case '#':
-            {
-                free_stack();
-                printf("stack has been cleared\n");
-                lock = 1;
-                break;
-            }
-            case '^':
-            {
-                push(pow(pop(), pop()));
-                break;
-            }
-            case '$':
-            {
-                push(sin(pop()));
-                break;
-            }
-            case '@':
-            {
-                push(exp(pop()));
-                break;
-            }
-            case '\n':
-            {
-                if (close_get_line || !lock)
-                {
-                    printf ("\t%.8g\n", pop());
-                    close_get_line = 0;
+    while ((len = get_line(s, MAXLINE)) > 0) {
+        while (len && (type = getop(s, digit)) != EOF) {
+            switch (type) {
+                case PRINT_TOP:
+                    print_top();
+                    if (is_EOF)
+                        goto normal_exit;
+                    printf(": ");
                     break;
-                }
-            }
-            default:
-            {
-                if (!lock)
-                {
-                    printf("error: unknown command %s\n", s);
-                    break;        
-                }
-                lock = 0;
-                break;
+                case CLEAR_STACK:
+                    clear_stack();
+                    if (is_EOF)
+                        goto normal_exit;
+                    printf(": ");
+                    break;
+                case DUPLICATE_STACK:
+                    duplicate_stack();
+                    printf(": ");
+                    if (is_EOF)
+                        goto normal_exit;
+                    break;
+                case COSINUS:
+                    push(cos(pop()));
+                    break;
+                case SINUS:
+                    push(sin(pop()));
+                    break;
+                case EXPONENT:
+                    push(exp(pop()));
+                    break;
+                case POW:
+                    op2 = pop();
+                    push(pow(pop(), op2));
+                    break;
+                case NUMBER:
+                    push(atof(digit));
+                    break;
+                case '+':
+                    push(pop() + pop());
+                    break;
+                case '*':
+                    push(pop() * pop());
+                    break;
+                case '-':
+                    op2 = pop();
+                    push(pop() - op2);
+                    break;
+                case  '/':
+                    op2 = pop();
+                    if (op2 != 0.0)
+                        push(pop() / op2);
+                    else
+                        printf("error: zero divisor\n");
+                    break;
+                case MODULUS:
+                    /* the data after the floating point will be discarded */
+                    op2 = pop();
+                    push((int)pop() % (int)op2);
+                    break;
+                case '\n':
+                    printf("\t= %.8g\n", pop());
+                    if (is_EOF)
+                        goto normal_exit;
+                    len = i = 0;
+                    printf(": ");
+                    break;
+                default:
+                    if (!isspace(type))
+                        printf("error: unknown command %s\n", s);
+                    break;
             }
         }
     }
+normal_exit:
+    printf("Exited by \"goto\" statement\n");
     return 0;
 }
 
-int getop(char buff_line[])
-{
-    extern int close_get_line;
-    int i = 0;
-    int c = 0;
-    static int length = 0;
+#define MAXVAL  100  /* maximum depth of val stack */
 
-    if (!close_get_line)
-    {
-        get_line(s, MAXVAL);
-        close_get_line = 1;
-        length = 0;
-    }
-    
-    while ((buff_line[0] = c = s[length]) == ' ' || c == '\t') // c = 1 c = 2
-    {
-        length++;
-    }
-    printf ("c is %c\n", c);
+int sp = 0;          /* next free stack position */
+double val[MAXVAL];  /* value stack */
 
-    if (c == '\n')
-    {
-        printf("'\\n' is found\n");
-    }
-    buff_line[1] = '\0';
-
-    if (c == '-')
-    {
-        length++;
-        while (isdigit(buff_line[i] = c = s[length]))
-        {
-            flag = -1;
-            i++;
-            length++;
-        }
-    }
-
-    if (!isdigit(c) && c  != '.' && c != ' ' && c != '\t' && c != EOF)
-    {
-        printf("%c is non-integer\n", c);
-        length++;
-        return c;
-    }
-    if (isdigit(c))
-    {
-        while (isdigit(buff_line[i] = c = s[length])) // c = s c = c
-        {
-            length++;
-            i++;
-        }
-    }
-
-    /*if (c == '.')
-    {
-        length++;
-        while(isdigit(s[length]))
-        {
-            length++;
-        }
-        length--;
-    } */
-    
-    buff_line[i] = '\0';
-    return NUMBER;
-}
-
+/* push:  push f onto value stack */
 void push(double f)
 {
     if (sp < MAXVAL)
-    {
-       val[sp] = (flag == -1) ? f * flag : f; 
-
-       if (val[sp] == -0.0)
-       {
-           val[sp] = 0.0;
-       }
-
-       sp++;
-       flag = 0;
-    }
+        val[sp++] = f;
     else
-    {
         printf("error: stack full, can't push %g\n", f);
-    }
 }
 
+/* pop:  pop and return top value from stack */
 double pop(void)
 {
     if (sp > 0)
-    {
         return val[--sp];
-    }
-    else
-    {
-        printf("error: stack empty\n");
+    else {
+        printf ("error: stack empty\n");
         return 0.0;
     }
 }
 
-double stack_top(void)
-{
-    return val[sp - 1];
-}
-void dublicate(void)
-{
-    val[sp] = val[sp - 1];
-    sp++;
-}
-void swap(void)
-{
-    double tmp = 0.0;
-    tmp = val[0];
-    val[0] = val[sp - 1];
-    val[sp - 1] = tmp;
-}
-void free_stack(void)
-{
-    for (int i = 0; i <= sp; i++)
-    {
-        val[i] = 0;
-    }
-    sp = 0;
-}
 
-void get_line(char *charline, int maxlen) 
+int get_line(char s[], int lim)
 {
-    int c = 0;
+    int j, c;
 
-    for (; --maxlen > 0 && (c = getchar()) != EOF && c!= '\n';)
-    {
-        *charline++ = c;
-    }
+    c = 0;
+    for (j = 0; j < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++j)
+        s[j] = c;
     if (c == '\n')
-    {
-        *charline++ = c;
-        *charline = '\0';
+        s[j++] = c;
+    if (c == EOF) {
+        s[j++] = '\n';
+        is_EOF++;
     }
-    if(c == EOF)
-    {
-       *charline = '\0';
-    }
+    s[j] = '\0';
+    return j;
 }
+
+/* getop:  get next character or numeric operand */
+int getop(char s[], char digit[])
+{
+    int j = 0, sym, is_variable;
+    while ((s[i]) == ' ' || s[i] == '\t')
+       i++;
+    is_variable = handle_variables(s);
+    sym = is_variable;
+    if (sym == '-' && !isdigit(s[++i]))
+        return sym;
+    else if (sym == '-' && isdigit(s[i]))
+        digit[j++] = sym;
+    if ((tolower(sym)) == 't')
+        return PRINT_TOP;
+    if (sym == 'c')
+        return CLEAR_STACK;
+    if (sym == 'C')
+        return COSINUS;
+    if (sym == 'S')
+        return SINUS;
+    if (sym == 'e')
+        return EXPONENT;
+    if ((tolower(sym) == 'd') && (s[++i]))
+        return DUPLICATE_STACK;
+    if (sym == '^' && (s[++i]))
+        return POW;
+    if (!isdigit(s[i]) && s[i] != '.')
+        return s[i++];      /* not a number */
+    if (isdigit(s[i]))    /* collect integer part */
+        while (isdigit(s[i]))
+            digit[j++] = s[i++];
+    if (s[i] == '.') {     /* collect fraction part */
+        digit[j++] = s[i++];
+        while (isdigit(s[i]))
+            digit[j++] = s[i++];
+    }
+    digit[j] = '\0';
+    return NUMBER;
+}
+
+void print_top(void)
+{
+    if (is_EOF)
+        putchar('\n');
+    if  (sp > 1)
+        printf("Top elements of stack: %.8g and %.8g\n", val[sp - 1], val[sp - 2]);
+    else if (sp)
+        printf("Top element of stack: %.8g\n", val[sp - 1]);
+    else
+        printf("error: no top element in stack\n");
+}
+
+void clear_stack(void)
+{
+    if (is_EOF)
+        putchar('\n');
+    do {
+        val[sp] = 0;
+    } while(--sp > 0);
+    sp = 0;
+    printf("stack has been cleared\n");
+}
+
+void duplicate_stack(void)
+{
+    int tmp_1 = 0, tmp_2 = sp;
+    if (is_EOF)
+        putchar('\n');
+    if (sp) {
+        do {
+            val[tmp_2++] =  val[tmp_1++];
+        } while(tmp_1 != sp);
+        sp = tmp_2;
+        printf("Stack has been duplicated.\n");
+    }
+    else
+        printf("error: nothing to duplicate.\n");
+}
+/*
+ * an example: 
+ *              a = 6 b = 5 +
+ *                          = 11
+ * */
+int handle_variables(char s[])
+{
+    int j = i;
+    if (!isalpha(s[i]))
+        return s[i];
+    while (!isdigit(s[++i]) && s[i] != '-' && s[i] != '\n' && s[i] != EOF)
+        ;
+    return (isdigit(s[i]) ? s[i] : s[j]);
+}
+
--- Chapter_4/4.13.c
+++ Chapter_4/4.13.c
@@ -1,18 +1,88 @@
 #include <stdio.h>
-#define SWAP(t, x, y) \
-{\
-    t temp = x;\
-    x = y;\
-    y = temp;\
-\
-    printf("%g %g", x, y);\
-}
+#include <ctype.h>
+#define MAXLINE 1000
 
-/*Define a macro swap(t,x,y) that interchanges two arguments of type t . (Block structure will help.)*/
+int atoi(char[]);
+int get_line(char line[], int lim);
+int getop(void);
 
-int main(void)
+#define swap(t, x, y) if (sizeof(t) == sizeof(x) && sizeof(t) == sizeof(y)) { \
+                            t = x, x = y, y = t; \
+                            } \
+                       else { \
+                            printf("Error: swap type is of %lu bytes, but x is of %lu type and y is of %lu type\n", sizeof(t), sizeof(x), sizeof(y)); \
+                            return -1; \
+                       } \
+
+static char line[MAXLINE] = {0};
+static int i = 0;
+
+main() 
 {
-    int ar_1 = 12;
-    int ar_2 = 9;
-    SWAP(int, ar_1, ar_2);
+    int x = 0, y = 0, t = 0, len = 0;
+    printf("Enter x and y values: ");
+  while ((len = get_line(line, MAXLINE))) {
+        if (len > 0) {
+            x = getop();
+            y = getop();
+            printf("Pre-swapped values: x = %d, y = %d\n", x, y);
+            swap(t, x, y);
+            printf("Post-swapped values: x = %d, y = %d\n", x, y);
+            i = 0;
+        }
+        printf("Enter x and y values: ");
+    }
+    return 0;
 }
+
+int get_line(char line[], int lim)
+{
+    int j, c;
+
+    c = 0;
+    for (j=0;j<lim-1 && (c = getchar())!=EOF && c!='\n'; ++j)
+        line[j] = c;
+
+    if (c == '\n')
+        line[j++] = c;
+
+    line[j] = '\0';
+    return j;
+}
+
+int atoi(char s[])
+{
+    int j, n, sign;
+
+    for (j = 0; isspace(s[j]); j++)  /* skip white space */
+        ;
+    sign = (s[j] == '-') ? -1 : 1;
+    if (s[j] == '+' || s[j] == '-')  /* skip sign */
+        j++;
+    for (n = 0; isdigit(s[j]); j++)
+        n = 10 * n + (s[j] - '0');
+    return sign * n;
+}
+
+int getop(void)
+{
+    int bufp = 0;
+    char digitBuf[MAXLINE] = {0};
+
+    while (isspace(line[i]))
+            i++;
+    while (isdigit(line[i]))
+        digitBuf[bufp++] = line[i++];
+    if (!bufp) {
+        i = 0;
+        printf("Enter the second digit: \n");
+        goto tryagain;
+    }
+    digitBuf[bufp] = '\0';
+    return atoi(digitBuf);
+
+tryagain:
+    get_line(line, MAXLINE);
+    return getop();
+}
+
