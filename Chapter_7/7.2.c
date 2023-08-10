ce109ef6b605fae36975c747dfe9801d3e7367a2
Alexdar19974 <voitexman11@gmail.com>
2023-08-10 08:06:27 +0700

    Refactored code of Chapter 7 according to K&R C-style

--- Chapter_7/7.2.c
+++ Chapter_7/7.2.c
@@ -1,30 +1,56 @@
 #include <stdio.h>
 #include <ctype.h>
-#define LIMIT 20
+#include <string.h>
+#define MAXLINE     1000
+#define LINEBREAK   10
+int get_line(char *, int);
+void break_string(char *, int);
 
-int main(void)
+main()
 {
-	char c = 0;
-	char counter = 0;
+    char str[MAXLINE] = {0};
+    int ret = 0, i;
 
-	while ((c = getchar()) != EOF)
-	{
-		if (counter == LIMIT)
-		{
-			puts("");
-			counter = 0;
-		}
-		if (iscntrl(c) || isspace(c))
-		{
-			printf(" 0x%x ", c);
-		}
-		else
-		{
-			putchar(c);
-		}
+    while ((ret = get_line(str, MAXLINE)) != EOF) {
+        if (ret >= LINEBREAK) {
+            break_string(str, LINEBREAK);
+            printf("Broken string:\n");
+            for (i = 0; str[i] != '\0'; i++)
+                !isgraph(str[i]) ? str[i] == '\n' ? printf("0x%x\n",str[i]) : printf("0x%x",str[i]) : putchar(str[i]);
+        } else {
+            printf("Unbroken string:\n");
+            for (i = 0; str[i] != '\0'; i++)
+                !isgraph(str[i]) ? str[i] == '\n' ? printf("0x%x\n",str[i]) : printf("0x%x",str[i]) : putchar(str[i]);
+        }
+    }
+    return 0;
+}
 
-		counter++;
-	}
 
-	return 0;
+int get_line(char *s, int lim)
+{
+    int i, c = 0;
+
+    for (i = 0; (i < lim) && ((c = getchar()) != EOF) && (c != '\n'); ++i)
+        *(s + i) = c;
+    if (c == '\n')
+        *(s + i) = c, i++; 
+    *(s + i) = '\0';
+    return c == EOF ? EOF : i;
+}
+
+void break_string(char *str, int lim)
+{
+    char tmp[MAXLINE] = {0};
+
+    if (str[lim] != '\n' && str[lim] != '\0') {
+        strcpy(tmp, &str[lim]);
+        str[lim] = '\n';
+        str[lim + 1] = '\0';
+        strcat(str, tmp);
+    } else if (str[lim] == '\n' || str[lim] == '\0')
+        return;
+
+    if ((int)(strlen(tmp) - 1) > LINEBREAK)
+        break_string(str, lim + LINEBREAK);
 }
--- Chapter_7/7.3.c
+++ Chapter_7/7.3.c
@@ -1,304 +1,145 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdarg.h>
 #include <ctype.h>
-#include <string.h>
+#define ON  1
+#define OFF 0
+#define OK 0
+#define MAXLINE 100
+enum errwarnCase { ERRNOMEM = -1, ERRARGNUM = -2, WARNARGNUM = -3, WARNNOARG = -4};
+int minprintf(int, char *, ...);
+void handle_err_warn(int, int);
+typedef unsigned int uint;
 
-#define FIELD_SIZE 100
-#define ARR_SIZE 100
-#define PRECISION_ON 1
-#define PRECISION_OFF 0
-
-void minprintf(char *fmt, ...);
-
-int main(void)
+main()
 {
-	int a = 1231;
-	int b = 124;
-	char c = 'A';
-	short s = 10;
-	int d = 1354253;
-	double f = 10.5;
-
-    minprintf("\t%10d \n%9d %c %h\n %s %.1f\n", a, b, c, s, "Hello, World!", f);
-
-	/*
-		В соответствии с ТЗ предоставляю минимальную реализацию функции minprintf:
-		1) работает с форматами типа integer, char, char * (string), float/double;
-		2) выводит на экран передаваемые значения в соответствии с ключами типа "%10d";
-		3) ключи работают только с %d и %f, поскольку это демонстративная версия;
-		4) функционал можно расширять, и она будет всё больше и больше становиться похожей на printf;
-	*/
-
+    int retVal = OK, argNum = 2;
+    printf("standard printf: ");
+    printf(":%.2s: %u %g\n", "Hello", 1, 5.7);
+    printf("      minprintf: ");
+    if ((retVal = minprintf(argNum,":%.2s: %u %g PUT HIM DOWN ON THE GROUND!\n", "Hello", 1, 5.7) != OK))
+        handle_err_warn(retVal, argNum);
     return 0;
 }
 
-/* minprintf: ограниченная версия printf
-   со списком аргументов переменной длины */
-
-void minprintf(char *fmt, ...)
+void handle_err_warn(int errWarn, int argNum)
 {
-    va_list ap; /* указатель на безымянные аргументы */
+    switch (errWarn)
+    {
+        case ERRNOMEM:
+            printf("error: failed to alloc %d for tmpBuf\n", MAXLINE);
+            break;
+        case ERRARGNUM:
+            printf("error: check the number of arguments.\n");
+            break;
+        case WARNARGNUM:
+            printf("warning: skipping one of the format specifiers due to number of args is %d\n", argNum);
+            break;
+        case WARNNOARG:
+            printf("warning: number of arguments=%d\n", argNum);
+            break;
+        case OK:
+        default:
+            break;
+    }
 
-    char *p = NULL;
-	char *sval = NULL;
-	char *f_w_arr = NULL;
-	char i = 0;
-	char field = 0;
-    int ival = 0;
-	char cval = 0;
-	short hval = 0;
-    double dval = 0.0;
-	char p_point = 0;
+}
 
-    va_start(ap, fmt); /* установить ap на 1-й аргумент без имени */
-    for (p = fmt; *p; p++)
-	{
-	    if (*p != '%')
-		{
-			if (field)
-        	{
-            	;
-        	}
-			else
-			{
-		    	putchar(*p);
-            	continue;
-			}
+/* minprintf: minimal printf with variable argument list */
+int minprintf(int arg_num, char *fmt, ...)
+{
+    va_list ap;     /* points to each unnamed arg in turn */
+    char *p, *tmpsval, *sval, *tmpBuf = NULL;
+    int ival, i = 0, lenLim = 0,retVal = 0;
+    uint uival;
+    double dval;
+
+    va_start(ap, fmt);
+
+    if (arg_num <= 0) {
+        va_end(ap);
+        retVal = WARNNOARG;
+        goto end;
+    }
+    if (!(tmpBuf = calloc(1, MAXLINE))) {
+        retVal = ERRNOMEM;
+        goto end;
+    }
+
+    va_start(ap, fmt); /* make ap point to 1st unnamed arg */
+    for (p = fmt; *p; p++) {
+        if (*p != '%' && *p != '.') {
+            if(*(p - 1) != '%')
+                putchar(*p);
+            else
+                retVal = WARNARGNUM;
+            continue;
         }
-
-		if (field)
-        {
-		   	p+=i;
-
-			field = 0;
-			i = 0;
-        }
-
-	    switch(*++p)
-		{
-        	case 'd':
-			{
-				if (*f_w_arr)
-				{
-					int num = atoi(f_w_arr);
-
-					switch(num)
-					{
-						case 10:
-						{
-                        	ival = va_arg(ap, int);
-							printf("%10d", ival);
-                   	 		break;
-						}
-						case 9:
-                        {
-                            ival = va_arg(ap, int);
-                            printf("%9d", ival);
-                            break;
-                        }
-						case 8:
-                        {
-                            ival = va_arg(ap, int);
-                            printf("%8d", ival);
-                            break;
-                        }
-						case 7:
-                        {
-                            ival = va_arg(ap, int);
-                            printf("%7d", ival);
-                            break;
-                        }
-						case 6:
-                        {
-                            ival = va_arg(ap, int);
-                            printf("%6d", ival);
-                            break;
-                        }
-                        case 5:
-                        {
-                            ival = va_arg(ap, int);
-                            printf("%5d", ival);
-                            break;
-                        }
-                        case 4:
-                        {
-                            ival = va_arg(ap, int);
-                            printf("%4d", ival);
-                            break;
-                        }
-                        case 3:
-                        {
-                            ival = va_arg(ap, int);
-                            printf("%3d", ival);
-                            break;
-                        }
-						case 2:
-                        {
-                            ival = va_arg(ap, int);
-                            printf("%2d", ival);
-                            break;
-                        }
-                        case 1:
-                        {
-                            ival = va_arg(ap, int);
-                            printf("%1d", ival);
-                            break;
-                        }
-						default:
-						{
-							ival = va_arg(ap, int);
-							printf("%10d", ival);
-						}
-					}
-					free(f_w_arr);
-					f_w_arr = NULL;
-					break;
-				}
-		    	ival = va_arg(ap, int);
-            	printf("%d", ival);
-            	break;
-			}
-        	case 'f':
-			{
-				if (*f_w_arr)
-                {
-                    int num = atoi(f_w_arr);
-
-                    switch(num)
-					{
-						case 10:
-                		{
-							dval = va_arg(ap, double);
-                    		printf("%.10f", dval);
-                    		break;
-                		}
-                		case 9:
-                		{
-							dval = va_arg(ap, double);
-             				printf("%.9f", dval);
-                			break;
-                		}
- 						case 8:
-                		{
-                  			dval = va_arg(ap, double);
-		                    printf("%.8f", dval);
-		                    break;
-		                }
-		                case 7:
-		                {
-		                	dval = va_arg(ap, double);
-		                    printf("%.7f", dval);
-		                    break;
-		                }
-		                case 6:
-		                {
-		                  	dval = va_arg(ap, double);
-		                    printf("%.6f", dval);
-		                    break;
-		                }
-		                case 5:
-		                {
-		                	dval = va_arg(ap, double);
-		                    printf("%.5f", dval);
-		                    break;
-		                }
-		                case 4:
-		                {
-		                	dval = va_arg(ap, double);
-		                    printf("%.4f", dval);
-		                    break;
-		                }
-						case 3:
-		                {
-		                	dval = va_arg(ap, double);
-		                    printf("%.3f", dval);
-		                    break;
-		                }
-		                case 2:
-		                {
-		               		dval = va_arg(ap, double);
-		                    printf("%.2f", dval);
-		                    break;
-		                }
-		                case 1:
-		                {
-		               		dval = va_arg(ap, double);
-		                	printf("%.1f", dval);
-		                    break;
-		                }
-		                default:
-		                {
-		               		dval = va_arg(ap, double);
-		                	printf("%f", dval);
-							break;
-		                }
-						p_point = PRECISION_OFF;
-						break;
-					}
-					free(f_w_arr);
-					f_w_arr = NULL;
-					break;
-				}
-				dval = va_arg(ap, double);
-				printf("%f", dval);
-				break;
-			}
-        	case 's':
-			{
-            	for (sval = va_arg(ap, char *); *sval; sval++)
-				{
-                	putchar(*sval);
-				}
-            	break;
-			}
-			case 'c':
-			{
-				cval = (char) va_arg(ap, int);
-				printf("%c", cval);
-				break;
-			}
-			case 'h':
-			{
-				hval = (short) va_arg(ap, int);
-				printf("%hu", hval);
-				break;
-			}
-	    	default:
-			{
-				if (*p == '.')
-				{
-					p_point = PRECISION_ON;
-					p++;
-				}
-				if (isdigit(*p))
-				{
-					f_w_arr = calloc(ARR_SIZE, sizeof(char));
-
-					for (char *tmp = p; isdigit(*tmp); tmp++, i++)
-					{
-						f_w_arr[i] = *tmp;
-					}
-
-					f_w_arr[i] = '\0';
-
-					while (*p != ' ' && *p != '\n' && *p != '\t' && *p != '.')
-					{
-						p--;
-					}
-					if (p_point == PRECISION_ON)
-					{
-						i--;
-					}
-					field = 1;
-					break;
-				}
-
-				printf("error: undefined specifier");
-				exit(EXIT_FAILURE);
-            	break;
-			}
+        if (!arg_num)
+            continue;
+jump_to_symbol:
+        switch (*++p) {
+            case 'u':
+                uival = va_arg(ap, unsigned int);
+                printf("%u", uival);
+                arg_num--;
+                break;
+            case 'i':
+            case 'd':
+                ival = va_arg(ap, int);
+                printf("%d", ival);
+                arg_num--;
+                break;
+            case 'f':
+                dval = va_arg(ap, double);
+                printf("%f", dval);
+                arg_num--;
+                break;
+            case 'g':
+                dval = va_arg(ap, double);
+                printf("%g", dval);
+                arg_num--;
+                break;
+            case 's':
+                sval = va_arg(ap, char *);
+                for (/*sval = va_arg(ap, char *),*/ tmpsval = sval; sval != NULL && *sval; sval++) {
+                    if (!*tmpBuf)
+                        putchar(*sval);
+                }
+                if (sval == NULL) {
+                    retVal = ERRARGNUM;
+                    goto end;
+                }
+                if (*tmpBuf)
+                    printf(lenLim == ON ? "%.*s" :"%*s", atoi(tmpBuf), tmpsval);
+                tmpBuf[0] = '\0', lenLim = OFF, arg_num--;
+                break;
+            case '*':
+                ival = va_arg(ap, int);
+                sprintf(tmpBuf, "%d", ival);
+                goto jump_to_symbol;
+                break;
+            case '.':
+                lenLim = ON;
+                goto jump_to_symbol;
+            default:
+                if (isdigit(*p) || (*p == '-' && isdigit(*(p + 1)))) {
+                    tmpBuf[i++] = *p++;
+                    while(isdigit(*p))
+                        tmpBuf[i++] = *p++;
+                    tmpBuf[i] = '\0', i = 0;
+                    p--;
+                    goto jump_to_symbol;
+                }
+                putchar(*p);
+                break;
         }
     }
-    va_end(ap); /* заврешающие операции  */
+end:
+    free(tmpBuf);
+    tmpBuf = NULL;
+    va_end(ap);    /* clean up when done */
+    return retVal;
 }
+
